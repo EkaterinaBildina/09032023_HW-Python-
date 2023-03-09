27897b22c19b51ecbc35e39fa18086e31a93d0df
EkaterinaBildina <kukresch@gmail.com>
2023-03-10 00:25:00 +0300

    change name HW001 to HW002

--- HW002.py
+++ HW002.py
@@ -2,3 +2,12 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+inputnumber = int(input("Введите трехзначне число:  "))
+
+if (inputnumber <= 999 and inputnumber >= 100):
+    num1 = int(inputnumber % 10)
+    num2 = int(inputnumber/10 % 10)
+    num3 = int(inputnumber/100 % 10)
+    print(f"{num1 + num2 + num3}")
+else:
+    print("Ошибка ввода числа!")
